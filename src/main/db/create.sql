4582630737ee55b8de86bb9cd365eb6541c597b5
BatuhanUlucay <hulucay@etu.edu.tr>
2019-06-11 19:06:37 +0300

    changed cookie data type

--- src/main/db/create.sql
+++ src/main/db/create.sql
@@ -5,13 +5,14 @@ CREATE TABLE ACCOUNT (
 );
 
 CREATE TABLE RECIPE (
-        Recipe_Id integer,
+        Recipe_Id char(36),
         Recipe_Name varchar(100),
         Recipe_Desc varchar(1000)
 );
 
 CREATE TABLE ACCOUNT_RECIPE (
         User_Id varchar(20),
-        Recipe_Id integer,
+        Recipe_Id char(36),
 	User_Cookie char(36)
 );
+
