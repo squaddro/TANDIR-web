2b7217d65362b4621031386cc5d85093ecbbd4a1
furkandinc <furkandinc@etu.edu.tr>
2019-06-12 14:28:00 +0300

    Folder orientation, readme update and cors support update

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # TANDIR-web
 TANDIR Server App
 
-First you should set you `DBASE_URL` value into environment table.
+First you should set your `DBASE_URL` value into environment table.
 
 Building
 `> mvn clean package`
 
 Executing
 `> java -jar target\web-server-1.0.jar`
+
+Then server will be running on `http://localhost:8080`
--- src/main/java/com/squadro/tandir/WebServer.java
+++ src/main/java/com/squadro/tandir/WebServer.java
@@ -9,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 public class WebServer {
+	private static final String[] origins = {"http://tandir.herokuapp.com", "http://localhost:8080"};
+	
 	public static void main(String[] args){
 		SpringApplication.run(WebServer.class, args);
 	}
@@ -17,15 +19,15 @@ public class WebServer {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/signin").allowedOrigins("*");
-				registry.addMapping("/signup").allowedOrigins("*");
-				registry.addMapping("/recipe").allowedOrigins("*");
-				registry.addMapping("/recipe/{id}").allowedOrigins("*");
-				registry.addMapping("/user").allowedOrigins("*");
-				registry.addMapping("/user/{id}").allowedOrigins("*");
-				registry.addMapping("/addrecipe").allowedOrigins("*");
-				registry.addMapping("/deleterecipe").allowedOrigins("*");
-				registry.addMapping("/updaterecipe").allowedOrigins("*");
+				registry.addMapping("/signin").allowedOrigins(origins);
+				registry.addMapping("/signup").allowedOrigins(origins);
+				registry.addMapping("/recipe").allowedOrigins(origins);
+				registry.addMapping("/recipe/{id}").allowedOrigins(origins);
+				registry.addMapping("/user").allowedOrigins(origins);
+				registry.addMapping("/user/{id}").allowedOrigins(origins);
+				registry.addMapping("/addrecipe").allowedOrigins(origins);
+				registry.addMapping("/deleterecipe").allowedOrigins(origins);
+				registry.addMapping("/updaterecipe").allowedOrigins(origins);
 			}
 		};
 	}
