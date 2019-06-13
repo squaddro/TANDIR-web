a3a57e13b25b8a6a7bf649f4fb69a65045ee232e
beyzanurgencetu <b.genc@etu.edu.tr>
2019-06-14 00:22:38 +0300

    added delete function

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,12 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+$('#signOut').click(function(){
+	  console.log(document.cookie);
+	});
+
+
+
 
 
 
--- src/main/resources/static/js/postActivity.js
+++ src/main/resources/static/js/postActivity.js
@@ -27,9 +27,8 @@ $(document).ready(function(){
     	}
     	userData.send(json_data);
     });
-});
+    
 
-$(document).ready(function(){
     $('#submit2').click(function(){
     	var userData = new XMLHttpRequest();
         var url = "https://tandir.herokuapp.com/signup";
@@ -56,9 +55,8 @@ $(document).ready(function(){
     	}
     	userData.send(json_data);
     });
-});
 
-$(document).ready(function(){
+    
     $('#createRecipe').click(function(){
     	var userData = new XMLHttpRequest();
     	var url = "https://tandir.herokuapp.com/addrecipe";
@@ -67,8 +65,8 @@ $(document).ready(function(){
     	userData.withCredentials = true;
     	var data = {};
         data.recipe_id = null;
-        data.recipe_name = document.getElementById('getName').value;
-        data.recipe_desc = document.getElementById('getRecipe').value;
+        data.recipe_name = document.getElementById('getName').value.replace(/\t/g, '');
+        data.recipe_desc = document.getElementById('getRecipe').value.replace(/\t/g, '');
         data.user_id = null;
     	var json_data = JSON.stringify(data);
     	userData.onload = function () {
@@ -87,5 +85,67 @@ $(document).ready(function(){
     	    }  		
     	}
     	userData.send(json_data);
+    });
+
+
+
+    $('#deleteRecipe').click(function(){
+    	
+    	var queryString = decodeURIComponent(window.location.search);
+    	queryString = queryString.substring(1);
+    	var queries = queryString.split("&");
+    	var queryString2 = "?" + queries[0]; 
+    	var userId = queries[0].substring(6); 
+    	
+    	var request = new XMLHttpRequest();
+        var url = "https://tandir.herokuapp.com/user/" + userId;
+    	request.open('GET', url, true);
+    	request.withCredentials = true;
+    	request.onload = function() {
+    	  // Begin accessing JSON data here
+    	  var data = JSON.parse(this.response);
+		  
+    	  if (request.status==200) {
+    		  var rec_id;
+              for(var i=0; i<data.recipes.length; i++){
+            	  if(document.getElementById('getDeletedRecipe').value.replace(/\t/g, '') === data.recipes[i].recipe_name){
+            	    rec_id = data.recipes[i].recipe_id;
+            	  }
+              }      
+              var recipeData = new XMLHttpRequest();
+          	var url = "https://tandir.herokuapp.com/deleterecipe";
+          	recipeData.open('POST', url, true);
+          	recipeData.setRequestHeader('Content-Type', 'application/json');
+          	recipeData.withCredentials = true;
+          	var data = {};
+              data.recipe_id = rec_id;
+              data.recipe_name = null;
+              data.recipe_desc = null;
+              data.user_id = null;
+          	var json_data = JSON.stringify(data);
+          	recipeData.onload = function () {
+          		
+          		if (recipeData.readyState == 4 && recipeData.status == 200) {
+          	        var response = JSON.parse(recipeData.responseText);
+          	        if(response.status == 111){
+                          alert(response.message);
+                          document.getElementById('getDeletedRecipe').value = '';
+                          location.reload();
+          	        }
+          	        else{
+          	        	alert(response.message);
+          	        }
+          	    }  		
+          	}
+          	recipeData.send(json_data);
+          	
+    	  } else {
+    	     alert('error');
+    	  }
+    	}
+    	request.send();
+    	
+    	
+        
     });
 });
